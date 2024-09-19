7f7c51b2f5b043bd5a4440f4e4cdc45b68f7388e
Emmalee06 <emmadhaliwal11@my.bcit.ca>
2024-09-19 15:42:14 -0700

    initialize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,9 @@
+console.log("horse!")
+let horseName = "LilLad"; 
+horseName = "Eevee"; //prefer double quotes
+let horseAge = 159;
+console.log(horseName); // this is a comment/ note
+console.log(horseAge);
+let isHorseInStable = true;
+console.log(isHorseInStable);
+isHorseInStable = false;
