0dad933e2ade8802885f28e7b7222800f38e4040
Emmalee06 <emmadhaliwal11@my.bcit.ca>
2024-09-26 14:37:52 -0700

    add operators to my stable

--- stables.js
+++ stables.js
@@ -7,3 +7,19 @@ console.log(horseAge);
 let isHorseInStable = true;
 console.log(isHorseInStable);
 isHorseInStable = false;
+
+ // basically typeof lets you know what you're using 
+
+const STABLE_MONTHLY_FEE = 100; 
+let horseAgeIn10Years = horseAge + 10;
+console.log(horseAgeIn10Years);
+console.log(STABLE_MONTHLY_FEE);
+let horseIntroduction = " is the name of my lovely horsie";
+//let rentIntroduction = "it costs " + STABLE_MONTHLY_FEE + " to board " + horseName;
+let rentIntroduction = `It costs ${STABLE_MONTHLY_FEE} to board ${horseName}`;
+let stableIntroduction = `Welcome to  "${horseName}" 's stable!`;
+console.log(stableIntroduction);
+console.log(horseName + horseIntroduction);
+console.log(rentIntroduction);
+
+
