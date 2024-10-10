b86a46f15dcc2a8b708d200e771fb320b32bb96e
Emmalee06 <emmadhaliwal11@my.bcit.ca>
2024-10-10 15:31:07 -0700

    lesson-6-more-horses

--- stables.js
+++ stables.js
@@ -21,3 +21,31 @@ if (isHorseInside) {
 } else { 
     console.log (horseNickname + " is playing outside!"); 
 }
+
+//LESSON 6 CODE
+// arrays structure -> ["name", age, isHorseInside]
+let horseOne = [horseName, horseAge, isHorseInside, horseNickname];
+let horseTwo= ["Snorlax", 5, true, "Snorsie"];
+let horseThree = ["Piplup", 3000, true, "Pips"];
+let areHorsesInside = true;
+
+
+let horseNames = [horseName, "Snorlax", "Piplup"];
+let horseAges = [horseAge, 5, 3000];
+let horseNicknames = [horseNickname, " Snorsie", " and Pips"];
+
+let horsesIntro = `There are ${horseNames.length} horses staying at my stable: ${horseNames[0]}, ${horseNames[1]}, and ${horseNames[2]}`
+
+console.log(horseOne);
+console.log(horseTwo);
+console.log(horseThree);
+
+console.log(horsesIntro);
+
+console.log(horseOne[0] + "'s nickname is " + horseOne[3]);
+console.log(`${horseTwo[0]}'s nickname is ${horseTwo[3]}`);
+console.log(`${horseThree[0]}'s nickname is ${horseThree[3]}`);
+
+if (areHorsesInside){
+    console.log(horseNicknames + " are staying inside the stable!");
+}
